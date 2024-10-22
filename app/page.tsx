dbc754e05f0b53c23cf36f95a16ecf70d4d8ef73
Rauljp16 <raul_astra16@hotmail.com>
2024-10-23 00:20:58 +0200

    components

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
+import Header from "@/components/Header";
 
 export default function Home() {
   return (
-    <p className="bg-zinc-800 p-9 text-3xl text-center text-amber-500 geist-sans font-bold">landing page</p>
+    <Header />
+    // <p className="bg-zinc-800 p-9 text-3xl text-center text-amber-500 geist-sans font-bold">landing page</p>
   );
 }

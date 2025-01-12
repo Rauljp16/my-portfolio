772e2029e934c20a0057031985b12b3beaa274b6
Rauljp16 <raul_astra16@hotmail.com>
2025-01-12 16:00:32 +0100

    about responsive terminado

--- components/About.tsx
+++ components/About.tsx
@@ -6,22 +6,22 @@ function About() {
     <div>
       <section
         id="about"
-        className="relative flex flex-col lg:flex-row h-svh items-center justify-center lg:justify-start gap-8 lg:gap-12 pt-32 px-10"
+        className="relative flex flex-col lg:flex-row lg:h-svh items-center justify-center lg:gap-12 pt-20 lg:pt-32 p-10"
       >
-        <div className="relative">
+        <div className="relative aspect-square rounded-full overflow-hidden w-4/6 md:w-[400px] lg:w-1/2 lg:max-w-[500px] lg:bottom-16">
           <Image
             src="/images/about.webp"
             alt="Logo"
             width={600}
             height={600}
-            className="w-32 md:w-40 -ml-1 pt-1 drop-shadow-blackSvg "
+            className="w-full h-full"
           />
         </div>
-        <div className="pb-8 max-w-[500px] lg:w-3/5">
-          <h1 className="relative lg:-left-64 lg:mb-12 lg:w-[600px] text-4xl tracking-wide sm:text-6xl md:text-7xl lg:text-[88px] font-train text-transparent bg-clip-text bg-gradient-to-r from-goldDark via-goldLight to-goldDark py-2 drop-shadow-blackSvg">
+        <div className="pb-8 lg:w-1/2 max-w-[800px]">
+          <h1 className="relative lg:-left-64 lg:mb-12 lg:w-[600px] text-4xl tracking-wide sm:text-6xl md:text-6xl lg:text-[88px] font-train text-transparent bg-clip-text bg-gradient-to-r from-goldDark via-goldLight to-goldDark py-4 drop-shadow-blackSvg">
             Sobre mí
           </h1>
-          <p className="text-sm sm:text-md md:text-lg tracking-wide leading-7 font-inter drop-shadow-white dark:drop-shadow-black transition duration-75 ease-linear max-w-full mb-10">
+          <p className="text-sm sm:text-md md:text-lg tracking-wide leading-7 font-inter drop-shadow-white dark:drop-shadow-black transition duration-75 ease-linear max-w-full lg:max-w-[500px] mb-10">
             Soy Raúl Jerez Pagán, <br />
             Full Stack Developer apasionado por crear aplicaciones bien
             estructuradas y centradas en el usuario. Disfruto cada etapa del
